7e34a2fab121c2bce2ce8ab93c713b72a2ef8b95
yessygc <yessgarciacampos@gmail.com>
2022-11-13 23:22:58 -0600

    Borrar suscripciones que ya no son válidas

--- server/push.js
+++ server/push.js
@@ -15,7 +15,7 @@ webpush.setVanidDetails(
 
 
 
-const suscripciones = require('./subs-db.json');
+let suscripciones = require('./subs-db.json');
 
 module.exports.getKey = () => {
     return urlsafeBase64.decode( vapid.publicKey );
@@ -35,13 +35,35 @@ module.exports.addSubscription = ( suscripcion ) => {
 
 module.exports.sendPush = ( post ) => {
 
+    console.log('Mandando PUSHES');
+
+    let notificacionesEnviadas = [];
+
 
     suscripciones.forEach( (suscripcion, i) => {
 
 
-        webpush.sendNotification( suscripcion, JSON.stringify( post ) );
+        const pushProm = webpush.sendNotification( suscripcion, JSON.stringify( post ) )
+            .then( console.log('Notificación enviada') )
+            .catch( err => {
+
+                console.log('Notificación falló');
+
+                if ( err.statusCode === 410 ) {
+                    suscripciones[i].borrar = true;
+                }
+            });
+
+            notificacionesEnviadas.push( pushProm );
     
     
+    });
+
+    Promise.all( notificacionesEnviadas ).then( () => {
+
+        suscripciones = suscripciones.filter( subs => !subs.borrar );
+    
+        fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify(suscripciones) );
     });
 
 
